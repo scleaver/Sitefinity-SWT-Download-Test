73da68a33c67c8c338338a7c906de80ec7874b5a
Seth Cleaver <seth@cleaver.id.au>
2022-12-08 13:47:41 +1000

    Sanity check

--- SitefinityWebApp/Api/AuthDownloadController.cs
+++ SitefinityWebApp/Api/AuthDownloadController.cs
@@ -3,12 +3,29 @@ using System.Linq;
 using System.Web.Http;
 using Telerik.Sitefinity.GenericContent.Model;
 using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Security.Claims;
 
 namespace SitefinityWebApp.Api
 {
     
     public class AuthDownloadController : ApiController
     {
+        [HttpGet]
+        [Authorize]
+        [Route("api/with-auth/user")]
+        public IHttpActionResult GetUser()
+        {
+            var identity = ClaimsManager.GetCurrentIdentity();
+
+            var user = new
+            {
+                UserId = identity.UserId,
+                Roles = identity.Roles.ToList()
+            };
+
+            return Ok(user);
+        }
+
         [HttpGet]
         [Authorize]
         [Route("api/with-auth/getdoc")]
